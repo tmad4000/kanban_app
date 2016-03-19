0025630143aa8d17cc27ca5e6dff000e1bfd5c2a
Jacob Cole <tmad4000@gmail.com>
2016-03-19 02:19:29 -0700

    adding notes ed 1

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,24 @@
 import React from 'react'
 
-export default ({task}) => <div>{task}</div>
+export default class Note extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      editing: false
+    }
+  }
+
+
+
+  render() {
+    if(this.state.editing) {
+      return <div>{this.props.task} edit</div>
+
+    }
+    else {
+      return <div onClick={() => this.setState({editing:true})}>{this.props.task}</div>
+    }
+
+  }
+
+}
